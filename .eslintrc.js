dd75bc2c40e84935f3852fe784ea98ff5b63b9ef
dedede <a925529@163.com>
2020-03-01 16:31:29 +0800

    sort rules in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,27 +19,18 @@ module.exports = {
     'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     semi: ['error', 'always'],
-    // 'no-return-await': 'off',
-    'template-curly-spacing': ['error', 'always'],
-    // 'space-before-function-paren': 'off',
-    // 'object-curly-spacing': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
-    // 'no-prototype-builtins': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    // '@typescript-eslint/no-var-requires': 'off',
-    // '@typescript-eslint/no-unused-vars': 'off',
-    // 'no-fallthrough': 'off',
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    'no-constant-condition': 'off',
-    'no-this-assignment': 'off',
-    '@typescript-eslint/no-this-alias': 'off',
-    'no-prototype-builtins': 'off',
     '@typescript-eslint/no-misused-promises': 'off',
     '@typescript-eslint/unbound-method': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-var-requires': 'off'
-    // '@typescript-eslint/member-delimiter-style': 'off',
-    // '@typescript-eslint/no-inferrable-types': 'off'
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-this-alias': 'off',
+    'template-curly-spacing': ['error', 'always'],
+    'no-constant-condition': 'off',
+    'no-this-assignment': 'off',
+    'no-prototype-builtins': 'off'
   }
 };
